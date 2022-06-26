e9774968c52de5733edbbb83f4b5d2fb4ff764b4
Arpine Avetisyan <106979662+arpineeee@users.noreply.github.com>
2022-06-26 18:53:35 +0400

    Create height.py

---
+++ height.py
@@ -0,0 +1,5 @@
+height = float(input("Enter your height with pounds: "))
+print(f'Your height with centimeters is: {12 * 2.54 * height} cm')
+
+height_inch = float(input("Enter your height with inches: "))
+print(f'Your height with centimeters is: {2.54 * height_inch} cm')
