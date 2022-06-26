eda12013328ad51759da55ee394437fc8264fd4b
Arpine Avetisyan <106979662+arpineeee@users.noreply.github.com>
2022-06-26 18:56:23 +0400

    Create circle_and_sphere.py

---
+++ circle_and_sphere.py
@@ -0,0 +1,6 @@
+pi = 3.14
+r = float(input("Enter the radius: "))
+area = pi * r**2
+volume = (4 * pi * r**3) / 3
+print(f'The area of your circle is: {area}')
+print(f'The volume of your ball is: {volume}')
