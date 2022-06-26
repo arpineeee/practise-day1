d7f48d394e243271e2a5bc6b61b03eb2b9ffd8d6
Arpine Avetisyan <106979662+arpineeee@users.noreply.github.com>
2022-06-26 18:43:07 +0400

    Create name_address.py

---
+++ name_address.py
@@ -0,0 +1,6 @@
+def name_address(name):
+     return name
+
+name = input("Enter your name: ")    
+address = input("Enter your address: ")
+print(f'My name is {name_address(name)} , my address is {name_address(address)})
