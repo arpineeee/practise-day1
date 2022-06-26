160096772779c812fca925389bd5df4bcc275cbc
Arpine Avetisyan <106979662+arpineeee@users.noreply.github.com>
2022-06-26 18:49:11 +0400

    Create souvenir_and_trifle.py

---
+++ souvenir_and_trifle.py
@@ -0,0 +1,3 @@
+souvenir =  int(input("Enter the count of your souvinirs: "))
+trifle =  int(input("Enter the number of your trifles: "))
+print(f'The  weight of your souvenirs and trifles is: {souvenir*75 + trifle*112}') 
